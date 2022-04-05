d079cc97d74216fcf25e812fe8e445dc5b27f286
Luis Garcia <77448672+luisgar289@users.noreply.github.com>
2022-04-05 17:56:33 -0500

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,13 +1,19 @@
 <?php
+    $serverApi = new ServerApi(ServerApi::V1);
     $client = new MongoDB\Client(
-        'mongodb+srv://<username>:luisgarcia15@<cluster-address>/test?retryWrites=true&w=majority'
-    );
-    $db = $client->registros;
-    $visita = $db->selectCollection($db,"registro_visitas");
+        'mongodb+srv://luisgarcia:luisgarcia15@cluster0.7nfcf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', [], ['serverApi' => $serverApi]);
+    $db = $client->test;
+    $collection = $db->users;
 
-    $nombre = $_POST["nombre"];
-    $correo = $_POST["correo"];
-    $edad = $_POST["edad"];
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['email'];
+    $edad = $_POST['edad'];
 
-    $nuevavisita->insertOne(array("nombre" => $nombre, "correo" => $correo, "edad" => $edad));
+    $document = [
+        'nombre' => $nombre,
+        'correo' => $correo,
+        'edad' => $edad
+    ];
+
+    $collection->insertOne($document);
 ?>
